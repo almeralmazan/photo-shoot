03588c5daa7e844f6bf3779248bd8132520384ed
Almer Almazan <almeralmazan@gmail.com>
2014-08-17 07:07:25 +0800

    Add reservations schema table

---
+++ app/database/migrations/2014_08_16_230258_create_reservations_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReservationsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::create('reservations', function($table)
+        {
+            $table->increments('id');
+            $table->string('name', 50);
+            $table->string('email', 100);
+            $table->string('contact_number', 20);
+            $table->integer('package_id');
+            $table->date('date')->default('0000-00-00');
+            $table->text('message');
+            $table->string('status');
+            $table->timestamps();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        Schema::drop('reservations');
+	}
+
+}
