17abbbd34bc300517b7d43b4255d59b5bd79793d
Almer Almazan <almeralmazan@gmail.com>
2014-08-17 11:44:21 +0800

    Add AdminController

---
+++ app/controllers/AdminController.php
@@ -0,0 +1,34 @@
+<?php
+
+class AdminController extends BaseController {
+
+    public function announcement()
+    {
+        $title = 'Admin Home Page';
+        return View::make('admin.announcement', compact('title'));
+    }
+
+    public function serviceEvent()
+    {
+        $title = 'Event Package';
+        return View::make('admin.services.event', compact('title'));
+    }
+
+    public function servicePhotoShoot()
+    {
+        $title = 'Photo Shoot Package';
+        return View::make('admin.services.photo-shoot', compact('title'));
+    }
+
+    public function serviceProduct()
+    {
+        $title = 'Products Package';
+        return View::make('admin.services.products', compact('title'));
+    }
+
+    public function gallery()
+    {
+        $title = 'Gallery Page';
+        return View::make('admin.gallery', compact('title'));
+    }
+}
