1334804ebf3ee9a1180dc0784ed0d2c6a1a7eaa4
Almer Almazan <almeralmazan@gmail.com>
2014-08-17 16:00:30 +0800

    Move login post method outside admin route group

--- app/routes.php
+++ app/routes.php
@@ -3,14 +3,14 @@
 // Public Pages
 Route::get('/', 'HomeController@index');
 Route::get('login', 'HomeController@login');
+Route::post('login', 'AdminController@verifyLogin');
 Route::get('services', 'HomeController@services');
 Route::get('galleries', 'HomeController@galleries');
 Route::get('about-us', 'HomeController@aboutUs');
 Route::get('contact', 'HomeController@contact');
 
-Route::group(['prefix' => 'admin'], function()
+Route::group(['before' => 'auth', 'prefix' => 'admin'], function()
 {
-    Route::post('login', 'AdminController@verifyLogin');
     Route::get('announcement', 'AdminController@announcement');
     Route::get('event', 'AdminController@serviceEvent');
     Route::get('photo-shoot', 'AdminController@servicePhotoShoot');
