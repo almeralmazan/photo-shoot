88c5d5951bd724ce809085d050b1163b08c58dde
Almer Almazan <almeralmazan@gmail.com>
2014-08-17 14:14:10 +0800

    Add home views

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -1,5 +1,17 @@
-@extends('layouts.main')
+@extends('layouts.admin')
 
 @section('content')
+
+<div class="services">
+    <h1>Our Services</h1>
+
+    @foreach ($services as $service)
+    <ul>
+        <li>
+            <a href="{{ URL::to('admin/service', [$service->id]) }}">{{ $service->name }}</a>
+        </li>
+    </ul>
+    @endforeach
+</div>
 
 @stop
