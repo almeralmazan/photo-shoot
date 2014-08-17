3a26a76cbf6806799c976e4c1b73d6c7e8c2e1fe
Almer Almazan <almeralmazan@gmail.com>
2014-08-17 09:16:41 +0800

    Add $fillable

--- app/models/User.php
+++ app/models/User.php
@@ -16,6 +16,8 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	 */
 	protected $table = 'users';
 
+    protected $fillable = ['username', 'password', 'remember_token'];
+
 	/**
 	 * The attributes excluded from the model's JSON form.
 	 *
