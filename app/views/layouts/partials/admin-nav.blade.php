7239a685a8e67c4182c8c70d3010e03aff3f7275
Almer Almazan <almeralmazan@gmail.com>
2014-08-17 15:23:27 +0800

    Add set_active in class

--- app/views/layouts/partials/admin-nav.blade.php
+++ app/views/layouts/partials/admin-nav.blade.php
@@ -14,16 +14,16 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
-                <li><a href="{{ URL::to('admin/announcement') }}">Announcement</a></li>
+                <li class="{{ set_active('admin/announcement') }}"><a href="{{ URL::to('admin/announcement') }}">Announcement</a></li>
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">Services <span class="caret"></span></a>
                     <ul class="dropdown-menu" role="menu">
-                        <li><a href="{{ URL::to('admin/event') }}">Event Package</a></li>
-                        <li><a href="{{ URL::to('admin/photo-shoot') }}">Photo Shoot</a></li>
-                        <li><a href="{{ URL::to('admin/products') }}">Products</a></li>
+                        <li class="{{ set_active('admin/event') }}"><a href="{{ URL::to('admin/event') }}">Event Package</a></li>
+                        <li class="{{ set_active('admin/photo-shoot') }}"><a href="{{ URL::to('admin/photo-shoot') }}">Photo Shoot</a></li>
+                        <li class="{{ set_active('admin/products') }}"><a href="{{ URL::to('admin/products') }}">Products</a></li>
                     </ul>
                 </li>
-                <li><a href="{{ URL::to('admin/gallery') }}">Gallery</a></li>
+                <li class="{{ set_active('admin/gallery') }}"><a href="{{ URL::to('admin/gallery') }}">Gallery</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 <li class="dropdown">
