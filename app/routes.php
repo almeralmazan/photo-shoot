bd1a2b008815b26e65c0d5dd6d1a16e84cad234e
Almer Almazan <almeralmazan@gmail.com>
2014-08-17 14:14:30 +0800

    Add admin routes

--- app/routes.php
+++ app/routes.php
@@ -2,8 +2,18 @@
 
 // Public Pages
 Route::get('/', 'HomeController@index');
+Route::get('login', 'HomeController@login');
 Route::get('services', 'HomeController@services');
 Route::get('galleries', 'HomeController@galleries');
 Route::get('about-us', 'HomeController@aboutUs');
 Route::get('contact', 'HomeController@contact');
 
+Route::group(['prefix' => 'admin'], function()
+{
+    Route::post('login', 'AdminController@verifyLogin');
+    Route::get('announcement', 'AdminController@announcement');
+    Route::get('event', 'AdminController@serviceEvent');
+    Route::get('photo-shoot', 'AdminController@servicePhotoShoot');
+    Route::get('product', 'AdminController@serviceProduct');
+    Route::get('gallery', 'AdminController@gallery');
+});
