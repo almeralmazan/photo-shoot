f87972c0444d64eb3b25214886fe147125ed3c62
Almer Almazan <almeralmazan@gmail.com>
2014-08-18 12:08:49 +0800

    Restrict links with admin and user

--- app/views/layouts/partials/admin-nav.blade.php
+++ app/views/layouts/partials/admin-nav.blade.php
@@ -14,6 +14,7 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
+                @if (Auth::user()->username == 'admin')
                 <li class="{{ set_active('admin/announcement') }}"><a href="{{ URL::to('admin/announcement') }}">Announcement</a></li>
                 <li class="{{ set_active('admin/reservation') }}"><a href="{{ URL::to('admin/reservation') }}">Reservation</a></li>
                 <li class="dropdown">
@@ -25,6 +26,9 @@
                     </ul>
                 </li>
                 <li class="{{ set_active('admin/gallery') }}"><a href="{{ URL::to('admin/gallery') }}">Gallery</a></li>
+                @elseif (Auth::user()->username == 'user')
+                <li class="{{ set_active('admin/reservation') }}"><a href="{{ URL::to('admin/reservation') }}">Reservation</a></li>
+                @endif
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 <li class="dropdown">
