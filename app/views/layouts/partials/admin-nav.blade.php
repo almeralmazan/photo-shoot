f7373884b6f8a798c081135325c0e2acadcfed4e
Almer Almazan <almeralmazan@gmail.com>
2014-08-20 10:12:38 +0800

    Change link name in navbar

--- app/views/layouts/partials/admin-nav.blade.php
+++ app/views/layouts/partials/admin-nav.blade.php
@@ -17,14 +17,7 @@
                 @if (Auth::user()->username == 'admin')
                 <li class="{{ set_active('admin/announcement') }}"><a href="{{ URL::to('admin/announcement') }}">Announcement</a></li>
                 <li class="{{ set_active('admin/reservation') }}"><a href="{{ URL::to('admin/reservation') }}">Reservation</a></li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Services <span class="caret"></span></a>
-                    <ul class="dropdown-menu" role="menu">
-                        <li class="{{ set_active('admin/event') }}"><a href="{{ URL::to('admin/event') }}">Event Package</a></li>
-                        <li class="{{ set_active('admin/photo-shoot') }}"><a href="{{ URL::to('admin/photo-shoot') }}">Photo Shoot</a></li>
-                        <li class="{{ set_active('admin/products') }}"><a href="{{ URL::to('admin/products') }}">Products</a></li>
-                    </ul>
-                </li>
+                <li class="{{ set_active('admin/services') }}"><a href="/admin/services">Services</a></li>
                 <li class="{{ set_active('admin/gallery') }}"><a href="{{ URL::to('admin/gallery') }}">Gallery</a></li>
                 @elseif (Auth::user()->username == 'user')
                 <li class="{{ set_active('admin/reservation') }}"><a href="{{ URL::to('admin/reservation') }}">Reservation</a></li>
