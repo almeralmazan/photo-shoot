0707990408a8789f752761ff71a9098d41661a8a
Almer Almazan <almeralmazan@gmail.com>
2014-08-20 10:11:47 +0800

    Indent

--- app/views/admin/gallery.blade.php
+++ app/views/admin/gallery.blade.php
@@ -5,17 +5,17 @@
     <div class="row">
         <div class="col-md-12">
 
-                <h3>Gallery</h3>
+            <h3>Gallery</h3>
 
-                {{ Form::open(['url' => 'admin/add/gallery-category', 'class' => 'form-inline']) }}
+            {{ Form::open(['url' => 'admin/add/gallery-category', 'class' => 'form-inline']) }}
 
-                <div class="form-group">
-                    {{ Form::text('gallery_name', null, ['class' => 'form-control', 'placeholder' => 'Gallery Name', 'required' => true]) }}
+            <div class="form-group">
+                {{ Form::text('gallery_name', null, ['class' => 'form-control', 'placeholder' => 'Gallery Name', 'required' => true]) }}
 
-                    <button type="submit" class="form-control btn btn-success"/>
-                        <span class="glyphicon glyphicon-plus"></span> Add Gallery </button>
-                </div>
-                {{ Form::close() }}
+                <button type="submit" class="form-control btn btn-success"/>
+                    <span class="glyphicon glyphicon-plus"></span> Add Gallery </button>
+            </div>
+            {{ Form::close() }}
 
             <br/>
 
@@ -32,7 +32,7 @@
                 <tr>
                     <td>{{ $gallery->name }}</td>
                     <td>{{ HTML::link('admin/gallery/' . $gallery->id, 'See Images', ['class' => 'btn btn-info']) }}</td>
-                    <td>{{ HTML::link('admin/delete/gallery/' . $gallery->id, 'delete', ['class' => 'btn btn-danger glyphi']) }}</td>
+                    <td>{{ HTML::link('admin/delete/gallery/' . $gallery->id, 'delete', ['class' => 'btn btn-danger']) }}</td>
                 </tr>
                 @endforeach
                 </tbody>
