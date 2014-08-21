b19724b45847381efce7dc6bdab2c0e938ca74b3
Almer Almazan <almeralmazan@gmail.com>
2014-08-21 14:36:55 +0800

    Add new assets

--- app/views/layouts/admin.blade.php
+++ app/views/layouts/admin.blade.php
@@ -13,8 +13,22 @@
 @yield('content')
 
 
-{{ HTML::script('js/jquery-1.10.2.min.js') }}
-{{ HTML::script('js/bootstrap.min.js') }}
-{{ HTML::script('js/script.js') }}
+
+<!-- jQuery Version 1.11.0 -->
+<script src="js/jquery-1.11.0.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+
+<!-- Metis Menu Plugin JavaScript -->
+<script src="js/plugins/metisMenu/metisMenu.min.js"></script>
+
+<!-- Custom Theme JavaScript -->
+<script src="js/sb-admin-2.js"></script>
+
+<!-- Custom Scripts -->
+<script src="js/script.js"></script>
+
+
 </body>
 </html>
