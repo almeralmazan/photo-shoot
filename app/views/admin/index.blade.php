97901deda1742b8eee5aa78a4983bc5cdc7324b8
Almer Almazan <almeralmazan@gmail.com>
2014-08-21 18:34:02 +0800

    Fix admin home page

--- app/views/admin/index.blade.php
+++ app/views/admin/index.blade.php
@@ -4,9 +4,9 @@
 <div class="row">
     <div class="col-md-12">
         <div class="jumbotron">
-            <h1>Jumbotron</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tincidunt est vitae ultrices accumsan. Aliquam ornare lacus adipiscing, posuere lectus et, fringilla augue. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tincidunt est vitae ultrices accumsan. Aliquam ornare lacus adipiscing.</p>
-            <p><a class="btn btn-primary btn-lg" role="button">Learn more</a>
+            <h1>Welcome to Dasboard Page</h1>
+            <p>
+               Just select from the navigation links to start editing and/or updating content.
             </p>
         </div>
     </div>
--- app/views/layouts/partials/admin-nav.blade.php
+++ app/views/layouts/partials/admin-nav.blade.php
@@ -8,7 +8,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="{{ URL::to('admin') }}">Printshoppe Admin</a>
+            <a class="navbar-brand" href="{{ URL::to('admin') }}">Printshoppe Dashboard</a>
         </div>
         <!-- /.navbar-header -->
 
